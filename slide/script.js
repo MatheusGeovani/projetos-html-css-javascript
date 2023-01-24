b4d94019b4e99a5745c60dbac636d26d21f14756
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-01-23 21:56:28 -0300

    projetos

---
+++ slide/script.js
@@ -0,0 +1,42 @@
+"use strict";
+const containerItens = document.querySelector("#container-itens")
+
+
+const images = [
+    {"id": "1", "url": "./img/chrono.jpg"},
+    {"id": "2", "url": "./img/inuyasha.jpg"},
+    {"id": "3", "url": "./img/tenchi.jpg"},
+    {"id": "4", "url": "./img/tenjhotenge.jpg"},
+    {"id": "5", "url": "./img/yuyuhakusho.jpg"},
+    {"id": "6", "url": "./img/ippo.png"},
+]
+
+const loadImages = (images, container) => {
+    images.forEach(image => {
+        container.innerHTML += `
+        <div class="item"> 
+            <img src="${image.url}"
+        </div>"`
+    })
+}
+
+
+loadImages(images, containerItens)
+let itens = document.querySelectorAll(".item")
+
+const previous = () => {
+    containerItens.appendChild(itens[0])
+    itens = document.querySelectorAll(".item")
+}
+
+const next = () => {
+    const lastItem = itens[itens.length - 1]
+    containerItens.insertBefore(lastItem, itens[0])
+    itens = document.querySelectorAll(".item")
+}
+
+
+document.querySelector("#previous").addEventListener("click", previous)
+document.querySelector("#next").addEventListener("click", next)
+
+
