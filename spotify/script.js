1d4e7aa68cb867edb1811ff00801ec23005f43e1
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-03-20 06:20:27 -0300

    music_spotify

--- spotify/script.js
+++ spotify/script.js
@@ -1,17 +1,60 @@
 const songName = document.querySelector("#songName");
 const bandName = document.querySelector("#bandName");
-let music = document.querySelector("#music");
+const music = document.querySelector("#music");
+const img = document.querySelector("#img");
 const play = document.querySelector(".play");
 const pause = document.querySelector(".pause");
-const back = document.querySelector(".back");
+const previous = document.querySelector(".previous");
 const next = document.querySelector(".next");
-let audio = ['Furioso Oceano.mp3','Pode Morar Aqui.mp3','Ruja o Leao.mp3','Vem Me Buscar.mp3','Vitorioso Es.mp3'];
-
-songName.innerHTML = 'Vem Me Buscar';
 
 
-back.addEventListener("click", function (){
+const Vem_Me_Buscar = {
+    songName: 'Vem Me Buscar',
+    artist: 'Jefferson e Suellen',
+    file: 'Vem_Me_Buscar'
+}
+const vitoriosoEs = {
+    songName: 'Vitorioso Es',
+    artist: 'Emi Sousa e André Aquino',
+    file: 'Vitorioso_Es'
+}
+const furiosoOceano = {
+    songName: 'Furioso Oceano',
+    artist: 'Jhonas Serra',
+    file: 'Furioso_Oceano'
+}
+const podeMorarAqui = {
+    songName: 'Pode Morar Aqui',
+    artist: 'Theo Rubia',
+    file: 'Pode_Morar_Aqui',
+}
+const rujaoLeao = {
+    songName: 'Ruja o Leao',
+    artist: 'Talita Catanzaro',
+    file: 'Ruja_o_Leao',
+}
 
+let playList = [Vem_Me_Buscar, vitoriosoEs, furiosoOceano, podeMorarAqui, rujaoLeao];
+let index = 2;
+
+function initializeSong(){
+    img.src = `img/${playList[index].file}.png`
+    music.src = `music/${playList[index].file}.mp3`
+    songName.innerText = `${playList[index].songName}`
+    bandName.innerText = `${playList[index].artist}`
+}
+
+initializeSong();
+
+previous.addEventListener("click", function (){
+    if(index == 0){
+        index = playList.length - 1;
+    }
+    else{
+        index -= 1
+    }
+    initializeSong();
+    music.play();
 })
 
 play.addEventListener("click", function (){
@@ -27,10 +70,18 @@ pause.addEventListener("click", function (){
 })
 
 
-
 next.addEventListener("click", function (){
     audio[0] += 1;
     music = audio;
     music.play()
 })
 
+next.addEventListener("click", function (){
+    if(index === playList.length - 1){
+        index = 0;
+    }else{
+        index += 1;
+    }
+    initializeSong();
+    music.play();
+})
