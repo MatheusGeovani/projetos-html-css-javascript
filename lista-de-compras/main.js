636fc73d11dcfb33c1d06b987c9b318a38a35621
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-04-05 22:22:30 -0300

    realizando o script da lista de compras

--- lista-de-compras/main.js
+++ lista-de-compras/main.js
@@ -1,14 +1,17 @@
 let listaDeItens = []
+let itemAEditar
 
 const form = document.getElementById('form-itens');
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
+const ulItensComprados = document.getElementById("itens-comprados")
 
 form.addEventListener("submit", function (evento) {
-    
+
     evento.preventDefault()
     salvarItem()
     mostrarItem()
+    itensInput.focus()
     itensInput.value = ""
 })
 
@@ -17,30 +20,79 @@ function salvarItem() {
     const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase()) // verificação com letra maiusculo e letra minuscula
     if (checarDuplicado) {
         alert("Já existe o item na lista")
-        itensInput.value = ""
-        return elemento.valor = ""
-
     } else {
         listaDeItens.push({
-            valor: comprasItem
+            valor: comprasItem,
+            checar: false
         })
     }
-    console.log(listaDeItens)
 }
 
 function mostrarItem() {
+    ulItens.innerHTML = ""
+    ulItensComprados.innerHTML = ""
     listaDeItens.forEach((elemento, index) => {
-        ulItens.innerHTML += `
+        if (elemento.checar) {
+            ulItensComprados.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" checked class="is-clickable" />  
+            <span class="itens-comprados is-size-5">${elemento.valor}</span>
+        </div>
+        <div>
+        <i class="fa-regular is-clickable fa-pen-to-square editar"></i>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+            `
+        } else {
+            ulItens.innerHTML += `
     <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
             <input type="text" class="is-size-5" value="${elemento.valor}"></input>
         </div>
         <div>
+            <button><i class="fa-regular fa-floppy-disk is-clickable"></button></i><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
         `
+        }
+    })
+    const inputCheck = document.querySelectorAll("input[type='checkbox']")
+
+    inputCheck.forEach(i => {
+        i.addEventListener("click", (evento) => {
+            const valorDoElemento = (evento.target.parentElement.parentElement.getAttribute("data-value"))
+            listaDeItens[valorDoElemento].checar = evento.target.checked
+            console.log(listaDeItens[valorDoElemento].checar)
+            mostrarItem()
+        })
+    })
+    const deletarObjetos = document.querySelectorAll(".deletar")
+
+    deletarObjetos.forEach(i => {
+        i.addEventListener("click", (evento) => {
+            const valorDoElemento = (evento.target.getAttribute("data-value"))
+            listaDeItens.splice(valorDoElemento, 1,)
+            mostrarItem()
+        })
+    })
+
+    const editarItens = document.querySelectorAll(".editar")
+    editarItens.forEach(i => {
+        i.addEventListener("click", (evento) => {
+            itemAEditar = (evento.target.parentElement.parentElement.getAttribute("data-value"))
+            mostrarItem()
+            
+
+        })
     })
 }
+function salvarEdicao(){
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+    console.log(itemEditado.value)
+}
+
 
