f2facee9109ba2f91f7944f72983a01ea7969a18
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-06-12 23:43:42 -0300

    requisicao

--- requisicoes-main/js/conectaApi.js
+++ requisicoes-main/js/conectaApi.js
@@ -1,9 +1,8 @@
-async function listaVideos (){
-    const conexao = await fetch("http://localhost:5501/videos")
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:5501/videos");
     const conexaoConvertida = await conexao.json();
-    
-    return conexaoConvertida
 
+    return conexaoConvertida;
 }
 
 export const conectaApi = {
