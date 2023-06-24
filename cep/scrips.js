09d32d0abbaa727a983ffe1af83703a772e2c29d
Matheus Geovani Alves de Medeiros <107431880+MatheusGeovani@users.noreply.github.com>
2023-06-24 18:57:26 -0300

    api-cep

---
+++ cep/scrips.js
@@ -0,0 +1,41 @@
+const button = document.querySelector(".button")
+const resultado = document.querySelector("#resultado")
+const inputCep = document.querySelector("#cep-input")
+const cep = inputCep.value
+
+function handKeyPress(event) {
+    if(event.key && event.key === "Enter"){
+        consultarCEP(event)
+    }
+}
+
+function consultarCEP(evento) {
+    evento.preventDefault()
+
+    const inputCep = document.querySelector("#cep-input")
+    const cep = inputCep.value
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+
+        .then(response => response.json())
+        .then(data => {
+            resultado.innerHTML = `<p><strong>Bairro</strong>: ${data.bairro}</p>
+        <p><strong>Cep</strong>: ${data.cep}</p>
+        <p><strong>Complemento</strong>: ${data.complemento}</p>
+        <p><strong>DDD</strong>: ${data.ddd}</p>
+        <p><strong>Localidade</strong>: ${data.localidade}</p>
+        <p><strong>Logradouro</strong>: ${data.logradouro}</p>
+        <p><strong>UF</strong>: ${data.uf}</p>`
+        })
+
+        
+        .catch(error => {
+            if(typeof cep === "string"){
+                alert("Digite apenas números para o CEP")
+            }
+            resultado.innerHTML = `<p>Não encontrado CEP informado</p>`
+        })
+}
+
+button.addEventListener("click", evento => consultarCEP(evento))
+inputCep.addEventListener("keypress", event => handKeyPress(event))
